49b380dfdad9bec0b29edb06f1e2e501c6d239dd
stixif <stixif@gmail.com>
2023-04-26 22:19:50 +0200

    Add files via upload

---
+++ aiocomfo_sensors.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+import asyncio
+import argparse
+import socket
+
+from aiocomfoconnect import discover_bridges
+from aiocomfoconnect import ComfoConnect
+from aiocomfoconnect.const import VentilationSpeed
+from aiocomfoconnect.sensors import SENSORS
+
+pin = 0
+local_name = 'FHEM'
+local_uuid = '00000000000000000000000000000010'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', help='fhem server address. (localhost)')
+parser.add_argument('--port', help='fhem telnet port. (7072)')
+parser.add_argument('--ip', help='ip address of the comfocontrol bridge (auto)')
+parser.add_argument('--fhemdummy', help='name of the fhem dummy (comfoconnect)')
+args = parser.parse_args()
+
+args.host = 'localhost' if args.host == None else args.host
+args.port = 7072 if args.port == None else args.port
+servhost = 'localhost'
+args.fhemdummy = "comfoconnect" if args.fhemdummy == None else args.fhemdummy
+
+bridge_ip = None
+bridge_uid = None
+newreading = None
+async def bridge_discovery():
+    """ ComfoConnect LAN C Bridge discovery example."""
+    
+    # Discover all ComfoConnect LAN C Bridges on the subnet.
+    bridges = await discover_bridges()
+    print(bridges) #gibt die bridge aus z.B. [<Bridge 192.168.1.100, UID=00000000002a10128003388fd71e111c>]
+
+    # Aufteilen des Ergebnisses in IP-Adresse und UID
+    output = str(bridges[0])
+    bridge_info = output.strip("[]")  # Entfernt eckige Klammern aus der Ausgabe
+    bridge_ip, bridge_uid = bridge_info.split(", UID=")  # Teilt die IP-Adresse und die UID auf
+    bridge_uid = bridge_uid.rstrip(">")
+    bridge_ip = bridge_ip.split(" ")[-1]  # Entfernt "Bridge" aus der IP-Adresse
+    
+    print(f"Bridge IP: {bridge_ip}")
+    print(f"Bridge UID: {bridge_uid}")
+    
+    return bridge_ip, bridge_uid
+
+async def main(local_uuid, bridge_ip, bridge_uid):
+    
+    bridge_ip, bridge_uid = await bridge_discovery()
+
+    ############# Create Outgoing FHEM connection ###################
+    print(args.host)
+    print(args.port)
+    fhem = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    fhem.connect((args.host, args.port))
+    
+    def fhemsend(msg):
+        ## fhem send here for comfoconnect requests
+        msg+="\r\n"
+        fhem.send(msg.encode('ascii'))
+        #print(f"{msg}")
+    
+    def setreading(var, value, unit):
+        var = var.replace(" ", "_")
+        cmd = "setreading %s %s %s" % (args.fhemdummy, str(var), str(value))
+        #print(f"{cmd} + {unit}")
+        fhemsend(cmd)
+
+        
+    def sensor_callback(sensor, value):
+        """ Print sensor updates. """
+        unit = sensor.unit if hasattr(sensor, 'unit') else None
+        setreading(sensor.name, value, sensor.unit)
+        print(f"{sensor.name} = {value} = {sensor.unit}")
+
+        
+    # Connect to the Bridge
+    comfoconnect = ComfoConnect(bridge_ip, bridge_uid, sensor_callback=sensor_callback)
+    await comfoconnect.connect(local_uuid)
+    
+    # Register all sensors
+    for key in SENSORS:
+        await comfoconnect.register_sensor(SENSORS[key])
+
+    # Wait 2 minutes so we can see some sensor updates
+    await asyncio.sleep(10)
+
+    # Disconnect from the bridge
+    fhem.close()
+    await comfoconnect.disconnect()
+
+    
+if __name__ == "__main__":
+        asyncio.run(main(local_uuid, bridge_ip, bridge_uid))  # Replace with your bridge's IP and UUID
---
+++ aiocomfo_set.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+
+import asyncio
+import argparse
+import socket
+
+from aiocomfoconnect import ComfoConnect, discover_bridges
+from aiocomfoconnect.const import (
+    VentilationBalance,
+    VentilationMode,
+    VentilationSetting,
+    VentilationTemperatureProfile,
+    VentilationSpeed,
+)
+from aiocomfoconnect.sensors import (
+    SENSOR_BYPASS_ACTIVATION_STATE,
+    SENSOR_OPERATING_MODE,
+    SENSOR_PROFILE_TEMPERATURE,
+    SENSORS,
+    Sensor as AioComfoConnectSensor,
+)
+
+
+pin = 0
+local_name = 'FHEM'
+local_uuid = '00000000000000000000000000000010'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ip', help='ip address of the comfocontrol bridge (auto)')
+parser.add_argument('--uuid', help='uuid of the comfocontrol bridge (auto)')
+parser.add_argument('--AIOType', help='Befehlstype')
+parser.add_argument('--AIOComm', help='Befehlswert')
+args = parser.parse_args()
+
+
+AIOType = None if args.AIOType == None else args.AIOType
+AIOComm = None if args.AIOComm == None else args.AIOComm
+bridge_ip = None if args.ip == None else args.ip
+bridge_uuid = None if args.uuid == None else args.uuid
+newreading = None
+
+
+async def bridge_discovery():
+    """ ComfoConnect LAN C Bridge discovery example."""
+    
+    # Discover all ComfoConnect LAN C Bridges on the subnet.
+    bridges = await discover_bridges()
+    print(bridges) #gibt die bridge aus z.B. [<Bridge 192.168.2.245, UID=00000000002a10128001144fd71e111c>]
+
+    # Aufteilen des Ergebnisses in IP-Adresse und UID
+    output = str(bridges[0])
+    bridge_info = output.strip("[]")  # Entfernt eckige Klammern aus der Ausgabe
+    bridge_ip, bridge_uuid = bridge_info.split(", UID=")  # Teilt die IP-Adresse und die UID auf
+    bridge_uuid = bridge_uuid.rstrip(">")
+    bridge_ip = bridge_ip.split(" ")[-1]  # Entfernt "Bridge" aus der IP-Adresse
+    
+    return bridge_ip, bridge_uuid
+    
+
+async def main(local_uuid, bridge_ip, bridge_uuid, AIOType, AIOComm):
+    """ Basic example."""
+    print(f"Bridge IP: {bridge_ip} -#- Bridge UID: {bridge_uuid}")
+    if bridge_uuid is None and bridge_ip is None:
+        bridge_ip, bridge_uuid = await bridge_discovery()
+    
+    # Connect to the Bridge
+    print(f"Bridge IP: {bridge_ip}")
+    print(f"Bridge UID: {bridge_uuid}")
+    
+    
+    comfoconnect = ComfoConnect(bridge_ip, bridge_uuid)
+    await comfoconnect.connect(local_uuid)
+    print(f"testen2")
+# Select command type based on AIOType argument
+    command = None
+    if AIOType == 'Ventilation_Mode':
+        command = comfoconnect.set_mode
+    elif AIOType == 'Bypass_Mode':
+        command = comfoconnect.set_bypass
+    elif AIOType == 'Balance_Mode':
+        command = comfoconnect.set_balance_mode
+    elif AIOType == 'Temperature_Profile':
+        command = comfoconnect.set_temperature_profile
+    elif AIOType == 'SPEED':
+        command = comfoconnect.set_speed
+    elif AIOType == 'BOOST':
+        command = comfoconnect.set_boost
+    elif AIOType == 'AWAY_Mode':
+        command = comfoconnect.set_away
+    elif AIOType == None:
+        command = None
+    else:
+        print(f"Invalid AIOType: {AIOType}")
+        await comfoconnect.disconnect()
+        return
+        
+
+    # Select command value based on AIOComm argument
+    if AIOType == 'Ventilation_Mode':
+        if AIOComm == 'AUTO':
+            value = VentilationMode.AUTO
+        elif AIOComm == 'MANUAL':
+            value = VentilationMode.MANUAL
+        else:
+            print(f"Invalid AIOComm for {AIOType}: {AIOComm}")
+            await comfoconnect.disconnect()
+            return
+        print(f"Valid AIOComm for {AIOType}: {AIOComm}")
+    elif AIOType == 'Bypass_Mode':
+        if AIOComm == 'AUTO':
+            value = VentilationSetting.AUTO
+        elif AIOComm == 'ON':
+            value = VentilationSetting.ON
+        elif AIOComm == 'OFF':
+            value = VentilationSetting.OFF
+        else:
+            print(f"Invalid AIOComm for {AIOType}: {AIOComm}")
+            await comfoconnect.disconnect()
+            return
+        print(f"Valid AIOComm for {AIOType}: {AIOComm}")
+    elif AIOType == 'Balance_Mode':
+        if AIOComm == 'BALANCE':
+            value = VentilationBalance.BALANCE
+        elif AIOComm == 'SUPPLY_ONLY':
+            value = VentilationBalance.SUPPLY_ONLY
+        elif AIOComm == 'EXHAUST_ONLY':
+            value = VentilationBalance.EXHAUST_ONLY
+        else:
+            print(f"Invalid AIOComm for {AIOType}: {AIOComm}")
+            await comfoconnect.disconnect()
+            return
+        print(f"Valid AIOComm for {AIOType}: {AIOComm}")
+    elif AIOType == 'Temperature_Profile':
+        if AIOComm == 'WARM':
+            value = VentilationTemperatureProfile.WARM
+        elif AIOComm == 'NORMAL':
+            value = VentilationTemperatureProfile.NORMAL
+        elif AIOComm == 'COOL':
+            value = VentilationTemperatureProfile.COOL
+        else:
+            print(f"Invalid AIOComm for {AIOType}: {AIOComm}")
+            await comfoconnect.disconnect()
+            return
+        print(f"Valid AIOComm for {AIOType}: {AIOComm}")
+    elif AIOType == 'SPEED':
+        if AIOComm == 'AWAY':
+            value = VentilationSpeed.AWAY
+        elif AIOComm == 'LOW':
+            value = VentilationSpeed.LOW
+        elif AIOComm == 'MEDIUM':
+            value = VentilationSpeed.MEDIUM
+        elif AIOComm == 'HIGH':
+            value = VentilationSpeed.HIGH
+        else:
+            print(f"Invalid AIOComm for {AIOType}: {AIOComm}")
+            await comfoconnect.disconnect()
+            return
+        print(f"Valid AIOComm for {AIOType}: {AIOComm}")
+        
+    elif AIOType == 'BOOST':
+        if AIOComm == 'OFF':
+            value = False
+        elif AIOComm is not None and AIOComm != '' and AIOComm.isdigit():
+            AIOComm = int(AIOComm)
+            value = f"{True}, timeout={AIOComm}"
+        else:
+            print(f"Invalid AIOComm for {AIOType}: {AIOComm}")
+            await comfoconnect.disconnect()
+            return
+        print(f"Valid AIOComm for {AIOType}: {AIOComm}")
+        print(f"Valid value {value}")
+        
+    elif AIOType == 'AWAY_Mode':
+        if AIOComm == 'OFF':
+            value = False
+        elif AIOComm is not None and AIOComm != '' and AIOComm.isdigit():
+            AIOComm = int(AIOComm)
+            value = f"{True}, timeout={AIOComm}"
+        else:
+            print(f"Invalid AIOComm for {AIOType}: {AIOComm}")
+            await comfoconnect.disconnect()
+            return
+        print(f"Valid AIOComm for {AIOType}: {AIOComm}")
+        print(f"Valid value {value}")
+
+    elif AIOType == None:
+        print(f"only Sensors")
+        await comfoconnect.disconnect()
+        return
+
+    # Execute the selected command
+    print(f"{command} and {value}")
+    await command(value)
+
+    # Disconnect from the bridge
+    await comfoconnect.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main(local_uuid, bridge_ip, bridge_uuid, args.AIOType, args.AIOComm))
