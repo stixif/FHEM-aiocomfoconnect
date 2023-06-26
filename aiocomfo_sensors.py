15fef8fafa5ccc5970d29f393e003717fb709494
stixif <stixif@gmail.com>
2023-06-26 17:09:56 +0200

    debug infos

--- aiocomfo_sensors.py
+++ aiocomfo_sensors.py
@@ -11,6 +11,8 @@ pin = 0
 local_name = 'FHEM'
 local_uuid = '00000000000000000000000000000010'
 
+debug = False
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', help='fhem server address. (localhost)')
 parser.add_argument('--port', help='fhem telnet port. (7072)')
@@ -32,7 +34,8 @@ async def bridge_discovery():
     
     # Discover all ComfoConnect LAN C Bridges on the subnet.
     bridges = await discover_bridges()
-    print(bridges) #gibt die bridge aus z.B. [<Bridge 192.168.1.100, UID=00000000002a10128003388fd71e111c>]
+    if debug:
+        print(bridges) #gibt die bridge aus z.B. [<Bridge 192.168.1.100, UID=00000000002a10128003388fd71e111c>]
 
     # Aufteilen des Ergebnisses in IP-Adresse und UID
     output = str(bridges[0])
@@ -40,9 +43,10 @@ async def bridge_discovery():
     bridge_ip, bridge_uuid = bridge_info.split(", UID=")  # Teilt die IP-Adresse und die UID auf
     bridge_uuid = bridge_uuid.rstrip(">")
     bridge_ip = bridge_ip.split(" ")[-1]  # Entfernt "Bridge" aus der IP-Adresse
-    
-    print(f"Bridge IP: {bridge_ip}")
-    print(f"Bridge UID: {bridge_uuid}")
+
+    if debug:
+        print(f"Bridge IP: {bridge_ip}")
+        print(f"Bridge UID: {bridge_uuid}")
     
     return bridge_ip, bridge_uuid
 
@@ -51,12 +55,14 @@ async def main(local_uuid, bridge_ip, bridge_uuid):
     if bridge_uuid is None and bridge_ip is None:
         bridge_ip, bridge_uuid = await bridge_discovery()
         
-    print(f"Bridge IP: {bridge_ip}")
-    print(f"Bridge UID: {bridge_uuid}")
+    if debug:
+        print(f"Bridge IP: {bridge_ip}")
+        print(f"Bridge UID: {bridge_uuid}")
 
     ############# Create Outgoing FHEM connection ###################
-    print(args.host)
-    print(args.port)
+    if debug:
+        print(args.host)
+        print(args.port)
     fhem = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     fhem.connect((args.host, args.port))
     
@@ -77,7 +83,8 @@ async def main(local_uuid, bridge_ip, bridge_uuid):
         """ Print sensor updates. """
         unit = sensor.unit if hasattr(sensor, 'unit') else None
         setreading(sensor.name, value, sensor.unit)
-        print(f"{sensor.name} = {value} = {sensor.unit}")
+        if debug:
+            print(f"{sensor.name} = {value} = {sensor.unit}")
 
         
     # Connect to the Bridge
