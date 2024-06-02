184fccd43cd8161c39669e97054f674dedeba888
stixif <stixif@gmail.com>
2024-06-02 20:44:23 +0200

    update 20240602

--- aiocomfo_sensors.py
+++ aiocomfo_sensors.py
@@ -1,107 +1,89 @@
 #!/usr/bin/python3
-import asyncio
-import argparse
-import socket
 
+import argparse
+import asyncio
+import socket
 from aiocomfoconnect import ComfoConnect, discover_bridges
-from aiocomfoconnect.const import VentilationSpeed
 from aiocomfoconnect.sensors import SENSORS
 
-pin = 0
-local_name = 'FHEM'
+# Lokale Konfigurationsparameter
 local_uuid = '00000000000000000000000000000010'
 
-debug = False
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--host', help='fhem server address. (localhost)')
-parser.add_argument('--port', help='fhem telnet port. (7072)')
-parser.add_argument('--ip', help='ip address of the comfocontrol bridge (auto)')
-parser.add_argument('--uuid', help='uuid of the comfocontrol bridge (auto)')
-parser.add_argument('--fhemdummy', help='name of the fhem dummy (comfoconnect)')
+# Kommandozeilenargumente
+parser = argparse.ArgumentParser(description='aiocomfoconnect sende Sensorwerte an FHEM')
+parser.add_argument('--Fhost', help='FHEM-Serveradresse (localhost)')
+parser.add_argument('--Fport', help='FHEM-Telnet-Port (7072)')
+parser.add_argument('--host', help='IP-Adresse der ComfoControl-Bridge')
+parser.add_argument('--uuid', help='UUID der ComfoControl-Bridge (auto)')
+parser.add_argument('--fhemdummy', help='Name des FHEM-Dummies (comfoconnect)')
 args = parser.parse_args()
 
-args.host = 'localhost' if args.host == None else args.host
-args.port = 7072 if args.port == None else args.port
-servhost = 'localhost'
-args.fhemdummy = "comfoconnect" if args.fhemdummy == None else args.fhemdummy
+args.Fhost = 'localhost' if args.Fhost is None else args.Fhost
+args.Fport = 7072 if args.Fport is None else int(args.Fport)
+args.fhemdummy = 'comfoconnect' if args.fhemdummy is None else args.fhemdummy
 
-bridge_ip = None if args.ip == None else args.ip
-bridge_uuid = None if args.uuid == None else args.uuid
-newreading = None
-async def bridge_discovery():
-    """ ComfoConnect LAN C Bridge discovery example."""
-    
-    # Discover all ComfoConnect LAN C Bridges on the subnet.
+async def discover_bridge():
+    # Automatisch die Bridge entdecken und deren UUID und IP auslesen.
     bridges = await discover_bridges()
-    if debug:
-        print(bridges) #gibt die bridge aus z.B. [<Bridge 192.168.1.100, UID=00000000002a10128003388fd71e111c>]
+    if bridges:
+        return bridges[0].uuid, bridges[0].host
+    return None, None
 
-    # Aufteilen des Ergebnisses in IP-Adresse und UID
-    output = str(bridges[0])
-    bridge_info = output.strip("[]")  # Entfernt eckige Klammern aus der Ausgabe
-    bridge_ip, bridge_uuid = bridge_info.split(", UID=")  # Teilt die IP-Adresse und die UID auf
-    bridge_uuid = bridge_uuid.rstrip(">")
-    bridge_ip = bridge_ip.split(" ")[-1]  # Entfernt "Bridge" aus der IP-Adresse
+def format_sensor_name(name):
+    return name.replace(" ", "_")
 
-    if debug:
-        print(f"Bridge IP: {bridge_ip}")
-        print(f"Bridge UID: {bridge_uuid}")
-    
-    return bridge_ip, bridge_uuid
+def fhemsend(fhem, msg):
+    msg += "\r\n"
+    fhem.send(msg.encode('ascii'))
 
-async def main(local_uuid, bridge_ip, bridge_uuid):
-    
-    if bridge_uuid is None and bridge_ip is None:
-        bridge_ip, bridge_uuid = await bridge_discovery()
-        
-    if debug:
-        print(f"Bridge IP: {bridge_ip}")
-        print(f"Bridge UID: {bridge_uuid}")
+def setreading(fhem, var, value, unit):
+    var = var.replace(" ", "_")
+    cmd = f"setreading {args.fhemdummy} {var} {value}"
+    fhemsend(fhem, cmd)
 
-    ############# Create Outgoing FHEM connection ###################
-    if debug:
-        print(args.host)
-        print(args.port)
+async def control_comfoconnect(params):
+    host = params.get('host')
+    uuid = params.get('uuid')
+
+    if not host or not uuid:
+        discovered_uuid, discovered_host = await discover_bridge()
+        uuid = uuid or discovered_uuid
+        host = host or discovered_host
+        if not uuid or not host:
+            print("Keine Bridge gefunden. Bitte geben Sie eine UUID und Host-IP an.")
+            return
+
     fhem = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    fhem.connect((args.host, args.port))
-    
-    def fhemsend(msg):
-        ## fhem send here for comfoconnect requests
-        msg+="\r\n"
-        fhem.send(msg.encode('ascii'))
-        #print(f"{msg}")
-    
-    def setreading(var, value, unit):
-        var = var.replace(" ", "_")
-        cmd = "setreading %s %s %s" % (args.fhemdummy, str(var), str(value))
-        #print(f"{cmd} + {unit}")
-        fhemsend(cmd)
+    fhem.connect((args.Fhost, args.Fport))
 
-        
     def sensor_callback(sensor, value):
-        """ Print sensor updates. """
-        unit = sensor.unit if hasattr(sensor, 'unit') else None
-        setreading(sensor.name, value, sensor.unit)
-        if debug:
-            print(f"{sensor.name} = {value} = {sensor.unit}")
+        # Callback-Funktion für Sensoraktualisierungen.
+        sensor_name = format_sensor_name(sensor.name)
+        sensor_unit = sensor.unit if hasattr(sensor, 'unit') else None
+        #print(f"{sensor_name}: {value} {sensor_unit}")
+        setreading(fhem, sensor_name, value, sensor_unit)
 
-        
-    # Connect to the Bridge
-    comfoconnect = ComfoConnect(bridge_ip, bridge_uuid, sensor_callback=sensor_callback)
-    await comfoconnect.connect(local_uuid)
-    
-    # Register all sensors
+    conn = ComfoConnect(host, uuid, sensor_callback=sensor_callback)
+    await conn.connect(local_uuid)
+
+    # Registrieren aller Sensoren
     for key in SENSORS:
-        await comfoconnect.register_sensor(SENSORS[key])
+        await conn.register_sensor(SENSORS[key])
 
-    # Wait 2 minutes so we can see some sensor updates
-    await asyncio.sleep(10)
-
-    # Disconnect from the bridge
-    fhem.close()
-    await comfoconnect.disconnect()
-
+    # Kurze Zeit warten, damit Sensordaten empfangen werden können
+    await asyncio.sleep(5)
     
-if __name__ == "__main__":
-        asyncio.run(main(local_uuid, bridge_ip, bridge_uuid))  # Replace with your bridge's IP and UUID
+    print(f"AIOcomfoconnect get FINISH")
+    fhem.close()
+    await conn.disconnect()
+
+def main():
+    print(f"AIOcomfoconnect get START")
+    params = {
+        'host': args.host,
+        'uuid': args.uuid
+    }
+    asyncio.run(control_comfoconnect(params))
+
+if __name__ == '__main__':
+    main()
